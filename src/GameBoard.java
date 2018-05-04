06f53aa697c74aabf8cd0bc37569bd7d48c054b4
Phu Truong <stevenztruong@yahoo.com>
2018-05-03 20:01:38 -0700

    Observer's skeleton

---
+++ src/GameBoard.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class GameBoard here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class GameBoard implements GameSubject
+{
+    // instance variables - replace the example below with your own
+
+
+    /**
+     * Constructor for objects of class GameBoard
+     */
+    public GameBoard()
+    {
+        // initialise instance variables
+
+    }
+
+}
---
+++ src/GameMonitor.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class GameMonitor here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class GameMonitor
+{
+
+
+
+    /**
+     * Constructor for objects of class GameMonitor
+     */
+    public GameMonitor()
+    {
+        // initialise instance variables
+
+    }
+
+}
